4ab9ce8216e5b72790be17c81d254467c9a3365f
Ongun <info@ongunakay.com>
2025-08-27 14:32:03 +0200

    initial Nuxt 3 project setup with Pinia, Bootstrap, TypeScript, and Vitest

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/image',
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/scripts',
+    '@nuxt/test-utils'
+  ]
+})
